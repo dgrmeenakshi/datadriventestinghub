f58a513d5e6283d1287dee44bb3cc82f2401744a
dgrmeenakshi@gmail.com <dgrmeenakhsi@gmail.com>
2021-12-24 21:40:48 +0530

    adding logs in test cases

--- src/test/java/com/DataDrivenTesting/base/TestBase.java
+++ src/test/java/com/DataDrivenTesting/base/TestBase.java
@@ -1,5 +1,6 @@
 package com.DataDrivenTesting.base;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,9 +9,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -24,6 +23,7 @@ public class TestBase {
     public static Properties config = new Properties();
     public static Properties OR = new Properties();
     public static FileInputStream fis;
+    public static Logger log = Logger.getLogger("devpinoyLogger");
 
     @BeforeSuite
     public void setUp() throws IOException {
@@ -31,13 +31,16 @@ public class TestBase {
 
             fis = new FileInputStream((System.getProperty("user.dir")) + "//src//main//resources//properties//Config.properties");
             config.load(fis);
+            log.debug("Config File loaded !!");
 
             fis = new FileInputStream((System.getProperty("user.dir")) + "//src//main//resources//properties//OR.properties");
             OR.load(fis);
+            log.debug("Object Repo File loaded !!");
         }
         if (config.getProperty("browser").equals("chrome")) {
             System.setProperty("webdriver.chrome.driver", (System.getProperty("user.dir")) + "//src//main//resources//executables//chromedriver.exe");
             driver = new ChromeDriver();
+            log.debug("chrome launched !!");
         } else if (config.getProperty("browser").equals("firefox")) {
             System.setProperty("webdriver.gecko.driver", "gecko.exe");
             driver = new FirefoxDriver();
@@ -46,6 +49,7 @@ public class TestBase {
             driver = new InternetExplorerDriver();
         }
         driver.get(config.getProperty("url"));
+        log.debug("Navigate to "+ config.getProperty("url"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
@@ -54,6 +58,7 @@ public class TestBase {
     public void tearDown() {
         if (driver != null) {
             driver.quit();
+            log.debug("Test execution completed !!!");
         }
     }
 }
--- src/test/java/com/DataDrivenTesting/testcases/LoginTest.java
+++ src/test/java/com/DataDrivenTesting/testcases/LoginTest.java
@@ -8,7 +8,9 @@ import org.testng.annotations.Test;
 public class LoginTest extends TestBase {
     @Test
     public void loginAsManager() throws InterruptedException {
+        log.debug("Inside Login Test");
         driver.findElement(By.cssSelector(OR.getProperty("bml"))).click();
-        Thread.sleep(9000);
+        Thread.sleep(1000);
+        log.debug("Logged in Successfully");
     }
 }
